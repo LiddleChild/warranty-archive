b87cbfa9e6a7ebaa7ea24cc4ae24f7da9bebc42a
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2023-12-06 22:28:18 +0700

    temp fix: next public env var not found in docker

--- packages/client/services/service.warranty.ts
+++ packages/client/services/service.warranty.ts
@@ -4,9 +4,7 @@ import { env } from "@/utils/util.env";
 
 export const getAllWarranties = async (): Promise<IWarranty[]> => {
   try {
-    const url = `http://localhost:${
-      process.env.NEXT_PUBLIC_BACKEND_PORT || ""
-    }/api/warranty`;
+    const url = `http://localhost:6544/api/warranty`;
     const data = await axios.get(url);
 
     return Promise.resolve(data.data as IWarranty[]);
