66f8b8550b7c8aaa8dd4e81780814e604d2ad1ce
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2023-12-07 18:25:40 +0700

    feat: express static

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import { ProcessEnv } from "./utils/util.env";
 import { postgresDataSource } from "./databases/db.postgres";
 import { WarrantyRepository } from "./repos/repo.warranty";
@@ -14,6 +15,7 @@ const main = async () => {
   const app = express();
   app.use(cors());
   app.use(express.json());
+  app.use("/", express.static(path.join(__dirname, "../../client/dist")));
 
   app.get("/api/warranty/", getAllWarranties);
 
--- packages/server/src/utils/util.env.ts
+++ packages/server/src/utils/util.env.ts
@@ -6,10 +6,10 @@ export class ProcessEnv {
   private constructor() {}
 
   public static DB_HOST = process.env.DB_HOST;
-  public static DB_PORT = parseInt(process.env.DB_PORT || "5555");
+  public static DB_PORT = parseInt(process.env.DB_PORT || "");
   public static DB_USERNAME = process.env.DB_USERNAME;
   public static DB_PASSWORD = process.env.DB_PASSWORD;
   public static DB_DATABASE = process.env.DB_DATABASE;
 
-  public static BACKEND_PORT = parseInt(process.env.BACKEND_PORT || "5556");
+  public static BACKEND_PORT = parseInt(process.env.BACKEND_PORT || "");
 }
