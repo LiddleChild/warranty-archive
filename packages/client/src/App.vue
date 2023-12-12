42738dffd88dfe8d62b7f0cf833085e5ab0935e9
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2023-12-12 16:23:40 +0700

    feat: remove headerview

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-import HeaderView from "./components/HeaderView.vue";
 import WarrantyTable from "./components/table/WarrantyTable.vue";
+import Create from "./components/Create.vue";
+import Logo from "./components/Logo.vue";
+import SearchField from "./components/SearchField.vue";
 </script>
 
 <template>
   <div
     class="w-screen h-screen bg-c-white overflow-x-hidden overflow-y-hidden flex flex-col"
   >
-    <HeaderView />
+    <div class="p-4 flex items-center gap-4">
+      <Logo />
+      <SearchField />
+      <Create />
+    </div>
     <div class="h-full px-4 overflow-y-scroll">
       <WarrantyTable />
     </div>
