1c31516a5e18165d229e5463f52f8e04d78f393f
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2023-12-17 16:39:07 +0700

    chore: change ui

--- packages/client/src/components/Add.vue
+++ packages/client/src/components/Add.vue
@@ -9,10 +9,7 @@ export default {
 </script>
 
 <template>
-  <button
-    @click="onClick"
-    class="bg-white rounded-full border-white broder-2 h-11 hover:bg-gray-200"
-  >
+  <button @click="onClick" class="bg-white rounded-full h-11 hover:bg-gray-200">
     <div class="flex items-center gap-1 p-2">
       <div
         class="material-symbols-outlined text-gray-400 cursor-pointer select-none"
